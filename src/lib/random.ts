1b4b83b463cf2e05409929e63b696931defa0925
dzzling <134210655+dzzling@users.noreply.github.com>
2024-02-25 17:39:03 +0100

    Revert "Implement random color generation"

--- src/lib/random.ts
+++
@@ -1,9 +0,0 @@
-export function getRandomInt(min: number, max: number) {
-	const minCeil = Math.ceil(min);
-	const maxFloor = Math.floor(max);
-	return Math.floor(Math.random() * (maxFloor - minCeil + 1)) + minCeil;
-}
-
-export function getRandomFloat(min: number, max: number) {
-	return Math.random() * (max - min) + min;
-}
