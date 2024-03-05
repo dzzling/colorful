8f6b84d0004de3bcb83fd1088745d2b1e2e1edea
Denise Müller <134210655+dzzling@users.noreply.github.com>
2024-03-05 12:48:01 +0100

    Button to component

---
+++ src/lib/Button.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { createEventDispatcher } from 'svelte';
+	import { page } from '$app/stores';
+	export let buttonText = 'Click me';
+
+	const dispatch = createEventDispatcher();
+	function nextScreen() {
+		dispatch('next');
+	}
+</script>
+
+<button
+	on:click={nextScreen}
+	class="flex flex-col justify-center items-center border-2 rounded-2xl border-black w-96 h-24"
+>
+	<p class="text-center text-lg">{buttonText}</p>
+</button>
