27f3d900fe8b1725afe00fb559158b11fda46316
Denise Müller <134210655+dzzling@users.noreply.github.com>
2024-03-05 16:27:05 +0100

    Implement tiles component

---
+++ src/lib/Tile.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import { type Color } from 'chroma-js';
+	import clsx from 'clsx';
+	import { createEventDispatcher } from 'svelte';
+	export let variant: 'NORMAL' | 'TARGET' | 'SELECTED' | 'WON' | 'LOST_CORRECT' | 'LOST_INCORRECT' =
+		'NORMAL';
+	export let tileColor: undefined | Color = undefined;
+	export let tileIndex: undefined | number = undefined;
+
+	const dispatch = createEventDispatcher();
+	function select() {
+		dispatch('select');
+	}
+</script>
+
+<div
+	class={clsx(
+		'w-full rounded-2xl h-full flex justify-center items-center outline outline-2 outline-black',
+		variant === 'SELECTED' && 'outline-4 outline-black outline-dashed',
+		(variant === 'TARGET' || variant === 'WON' || variant === 'LOST_INCORRECT') &&
+			'outline outline-4 outline-black'
+	)}
+	style="background-color: {tileColor};"
+	on:click={() => select()}
+>
+	{#if variant === 'TARGET'}
+		<img src="screen-normal.svg" alt="Target" width="40%" height="40%" />
+	{:else if variant === 'WON' || variant === 'LOST_CORRECT'}
+		<img src="check-circle.svg" alt="Target" width="40%" height="40%" />
+	{:else if variant === 'LOST_INCORRECT'}
+		<img src="x-circle.svg" alt="Selected color" width="40%" height="40%" />
+	{/if}
+</div>
